0ebb4779236a07a59f1f826769e08cb5846370f6
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-04-26 22:41:14 +0100

    solved first 4 algorithms

--- 01-log-even-nums/Unsolved/log-even-nums.js
+++ 01-log-even-nums/Unsolved/log-even-nums.js
@@ -1,4 +1,15 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
-var logEvenNums = function(num) {};
+var logEvenNums = function(num) {
+    for (var i = 0; i <= num; i++){
+        var currentNumber = i;
+
+        if (currentNumber % 2 === 0){
+            console.log(currentNumber);
+        } 
+    }   
+};
+
+
+
