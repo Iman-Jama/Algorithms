d3da6fa1b00f4e96dd64db238ddaef749f36db1b
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-04-27 18:21:34 +0100

    Solved algorithm in file 02-max-num

--- 02-max-num/Unsolved/max-num.js
+++ 02-max-num/Unsolved/max-num.js
@@ -1,3 +1,10 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    let finalNumber = arr[0];
+    for (var i = 0; i<arr.length; i++){
+        if (arr[i]>finalNumber) {
+            finalNumber = arr[i]
+        }
+    } return finalNumber;
+};
