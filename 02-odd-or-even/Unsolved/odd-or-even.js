0b3c0ed18b2aaa4cfa3e96493d67b3cdff25fbea
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-05-30 17:56:16 +0100

    updated oddEven algorithm

--- 02-odd-or-even/Unsolved/odd-or-even.js
+++ 02-odd-or-even/Unsolved/odd-or-even.js
@@ -1,4 +1,14 @@
 // Write code to return the string "odd" if `num` is an odd number
 // and return the string "even" if `num` is an even number
 
-var oddOrEven = function(num) {};
+var oddOrEven = function(num) {
+
+    for (var i = 0; i < num.length; i++) {
+        let testNum = num[i];
+        if (testNum % 2 === 0) {
+            return "even"
+        }
+        
+    } return "odd";
+};
+
